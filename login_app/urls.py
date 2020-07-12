bbe0250ceb7095c7c302afdbb8f8db220b0cd9b3
Shari Bradford <shari.bradford@gmail.com>
2020-07-12 02:00:45 -0400

    added birthday to model, additional login/register functionality

--- login_app/urls.py
+++ login_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('register', views.register),
     path('login', views.login),
+    path('logout', views.logout),
+    path('success', views.welcome),
 ]
